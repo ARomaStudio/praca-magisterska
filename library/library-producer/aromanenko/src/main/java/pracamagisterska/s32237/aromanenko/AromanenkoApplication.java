368d6837495079dd42e09c85337677d466ab7a21
Artem Romanenko <Artem.Romanenko@allianz.pl>
2024-06-22 11:52:37 +0200

    library initial commit

---
+++ library/library-producer/aromanenko/src/main/java/pracamagisterska/s32237/aromanenko/AromanenkoApplication.java
@@ -0,0 +1,13 @@
+package pracamagisterska.s32237.aromanenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromanenkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromanenkoApplication.class, args);
+	}
+
+}
